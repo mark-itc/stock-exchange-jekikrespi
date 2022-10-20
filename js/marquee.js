9b95bf0e22d2be9019c367e46579d3fe81abac78
Jeki <jekikrespi@gmail.com>
2022-10-20 20:32:19 +0300

    end of milestone5

--- js/marquee.js
+++ js/marquee.js
@@ -1,25 +1,28 @@
-async function priceApi(url) {
-  try {
-    let response = await fetch(url);
-    let result = await response.json();
-    return result;
-  } catch (err) {
-    console.log("err: ", err);
+class Marquee {
+  constructor(marqueeDiv) {
+    this.marqueeDiv = marqueeDiv;
+  }
+  async priceApi(url) {
+    try {
+      let response = await fetch(url);
+      let result = await response.json();
+      return result;
+    } catch (err) {
+      console.log("err: ", err);
+    }
+  }
+  async load() {
+    this.result = await this.priceApi(
+      "https://stock-exchange-dot-full-stack-course-services.ew.r.appspot.com/api/v3/quotes/nasdaq/"
+    );
+
+    this.result.forEach((element) => {
+      console.log(element.name);
+      console.log(element.price);
+      this.marqueeDiv.innerHTML += element.name;
+      this.marqueeDiv.innerHTML += "  ";
+      this.marqueeDiv.innerHTML += element.price;
+      this.marqueeDiv.innerHTML += "  ";
+    });
   }
 }
-async function companyPrice() {
-  let result = await priceApi(
-    "https://stock-exchange-dot-full-stack-course-services.ew.r.appspot.com/api/v3/quotes/nasdaq/"
-  );
-
-  result.forEach((element) => {
-    console.log(element.name);
-    console.log(element.price);
-    const marquee = document.getElementById("marqueeText");
-    marquee.innerHTML += element.name;
-    marquee.innerHTML += "  ";
-    marquee.innerHTML += element.price;
-    marquee.innerHTML += "  ";
-  });
-}
-companyPrice();
