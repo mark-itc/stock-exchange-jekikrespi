102ef79913fbc9b9728aabcc9ad89a54801a6b17
Jeki <jekikrespi@gmail.com>
2022-10-22 22:15:41 +0300

    end of milestone6-7

--- js/marquee.js
+++ js/marquee.js
@@ -2,27 +2,34 @@ class Marquee {
   constructor(marqueeDiv) {
     this.marqueeDiv = marqueeDiv;
   }
+
+  async load() {
+    this.result = await this.priceApi(
+      "https://stock-exchange-dot-full-stack-course-services.ew.r.appspot.com/api/v3/quotes/NASDAQ"
+    );
+
+    this.result.forEach((company) => {
+      this.price = `$${parseFloat(company.price).toFixed(2)}`;
+      this.priceSpan = document.createElement("span");
+      this.priceSpan.style.color = "Green";
+      this.priceSpan.innerHTML = this.price;
+      this.symbolSpan = document.createElement("span");
+      this.symbolSpan.innerHTML = company.symbol;
+      this.companyDiv = document.createElement("span");
+      this.companyDiv.classList.add("company-div");
+      this.companyDiv.appendChild(this.symbolSpan);
+      this.companyDiv.appendChild(this.priceSpan);
+      this.marqueeDiv.appendChild(this.companyDiv);
+    });
+  }
+
   async priceApi(url) {
     try {
-      let response = await fetch(url);
-      let result = await response.json();
-      return result;
+      this.response = await fetch(url);
+      this.result = await this.response.json();
+      return this.result;
     } catch (err) {
       console.log("err: ", err);
     }
-  }
-  async load() {
-    this.result = await this.priceApi(
-      "https://stock-exchange-dot-full-stack-course-services.ew.r.appspot.com/api/v3/quotes/nasdaq/"
-    );
-
-    this.result.forEach((element) => {
-      console.log(element.name);
-      console.log(element.price);
-      this.marqueeDiv.innerHTML += element.name;
-      this.marqueeDiv.innerHTML += "  ";
-      this.marqueeDiv.innerHTML += element.price;
-      this.marqueeDiv.innerHTML += "  ";
-    });
   }
 }
